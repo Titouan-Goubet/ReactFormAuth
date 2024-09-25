59eb64b7d6d5ff4688ab5a9f996d6d87aff7e6d4
Titouan Goubet <titouangoubet@hotmail.fr>
2024-09-25 20:05:04 +0200

    Modif des url pour la prod

--- src/signup/signup.jsx
+++ src/signup/signup.jsx
@@ -22,16 +22,19 @@ export default function Signup() {
   const onSubmit = async (data) => {
     setIsLoading(true);
     try {
-      const response = await fetch("http://localhost:8080/api/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: data.email,
-          password: data.password,
-        }),
-      });
+      const response = await fetch(
+        "https://javaformauth-production.up.railway.app/api/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: data.email,
+            password: data.password,
+          }),
+        }
+      );
 
       const result = await response.json();
 
